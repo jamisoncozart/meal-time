a5208132435b570a49c2cd4a84d221acf1c45eae
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-21 12:21:04 -0800

    Refactor code

--- mealTime.cs
+++ mealTime.cs
@@ -2,23 +2,17 @@ using System;
 
 class MealTime
 {
-  static void AskWhatYouAteFor(string meal)
-  {
-    Console.WriteLine("What did you eat for " + meal + "?");
-  }
-
   static void Main()
   {
     AskWhatYouAteFor("breakfast");
-    string yourBreakfast = Console.ReadLine();
-    Console.WriteLine("You had " + yourBreakfast + " for breakfast.");
-
     AskWhatYouAteFor("lunch");
-    string yourLunch = Console.ReadLine();
-    Console.WriteLine("You had " + yourLunch + " for lunch.");
-
-    AskWhatYouAteFor("dinner");
-    string yourDinner = Console.ReadLine();
-    Console.WriteLine("You had " + yourDinner + " for Dinner.");
+    AskWhatYouAteFor("dinner");    
+  }
+  
+  static void AskWhatYouAteFor(string meal)
+  {
+    Console.WriteLine("What did you eat for " + meal + "?");
+    string yourMeal = Console.ReadLine();
+    Console.WriteLine("You had " + yourMeal + " for " + meal + ".");
   }
 }
